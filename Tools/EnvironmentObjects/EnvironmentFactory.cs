15cf348d984a689bebd3463aa0515aeaf7ef6537
John Cook <74627489+jjocook@users.noreply.github.com>
2022-09-26 17:03:21 -0400

    enviro factory progress

--- Tools/EnvironmentObjects/EnvironmentFactory.cs
+++ Tools/EnvironmentObjects/EnvironmentFactory.cs
@@ -4,9 +4,58 @@
 
 class environmentFactory
 {
-    IEnvironment GetEnvironment(int environment)
+    private IEnvironment GetEnvironment(Environment environment)
     {
+        switch (environment)
+        {
+            case Environment.Statues:
+                return new Statue();
 
-        return new Statue();
+            case Environment.SquareBlock:
+                return new Statue();
+
+            case Environment.PushBlock:
+                return new Statue();
+
+            case Environment.Fire:
+                return new Statue();
+
+            case Environment.WalkableBlueGap:
+                return new Statue();
+                
+            case Environment.Stairs:
+                return new Statue();
+                
+            case Environment.WhiteBrick:
+                return new Statue();
+                
+            case Environment.Ladders:
+                return new Statue();
+                
+            case Environment.BlueFloor:
+                return new Statue();
+                
+            case Environment.BlueSand:
+                return new Statue();
+            
+            case Environment.RoomBorder:
+                return new Statue();
+
+            case Environment.OpenDoor:
+                return new Statue();
+
+            case Environment.BombedWallOpening:
+                return new Statue();
+
+            case Environment.KeyholeLockedDoor:
+                return new Statue();
+
+            case Environment.DiamondSymbolLockedDoor:
+                return new Statue();
+            
+            default:
+                return new Statue();
+        }
+        
     }
 }
