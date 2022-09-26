5484c77b972cae71f360ce2bb3ca32a198bf796e
TeiBAU5 <li_jianyao@live.com>
2022-09-26 17:57:52 -0400

    Update Interfaces.cs

--- Tools/Interfaces/Interfaces.cs
+++ Tools/Interfaces/Interfaces.cs
@@ -9,10 +9,6 @@ interface ISprite
 {
     void Update(int x, int y);
     //void initialize();
-
-<<<<<<< HEAD:Tools/Interfaces.cs
-    void Draw(SpriteBatch spriteBatch);
-=======
     void draw(SpriteBatch spriteBatch);
 }
 
@@ -20,5 +16,4 @@ interface IEnvironment
 {
     void update();
     void draw(SpriteBatch spriteBatch);
->>>>>>> b019a0ed3ef1143d86ffc0a63535bea97b9edca4:Tools/Interfaces/Interfaces.cs
 }
