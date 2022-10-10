c8050e3679810801606e967c041d736061c25c7e
Ben Elleman <elleman.28@buckeyemail.osu.edu>
2022-10-10 18:25:58 -0400

    Conceptual blueprint has been further developed.

--- Tools/Collision.cs
+++ Tools/Collision.cs
@@ -12,25 +12,28 @@ namespace LOZ.Tools
 {
     internal class Collision
     {
-        bool Intersects(Rectangle a, Rectangle b)
+        bool CollisionChecker(Rectangle a, Rectangle b)
         {
             return a.Intersects(b);
         }
-        void CollisionChecker(Object a, Object b)
+
+        void CollisionDirective(Object a, Object b)
         {
-            //This code could be simplified if it can be guranteed Link will always be the 1st (or last) object in the array of collidable objects
             if (a is IEnvironment)
             {
-/*                if (b is Enemy b1) collide(b1 rect);
-                else if (b is Link b2) collide(b2 rect);*/
+/*                if (b is Enemy b1) collide(a rect, b1 rect);*/
             }
             if (b is IEnvironment)
             {
-/*                if (a is Enemy a1) collide(a1 rect);
-                else if (a is Link a2) collide(a2 rect);*/
+/*                if (a is Enemy a1) collide(b rect, a1 rect);
+                else if (a is Link a2) collide(b rect, a2 rect);*/
             }
             else if (a is Link linka && b is Enemy) linka.Damage();
-            else if (a is Enemy && b is Link linkb) linkb.Damage();
+        }
+
+        void Collide(Rectangle unchanged, Rectangle changed)
+        {
+
         }
     }
 }
