ee43362b9e8495d3d143df7c735a55eb8d448158
John Cook <johnocook@gmail.com>
2022-10-17 11:24:28 -0400

    started on level loading tool

---
+++ Tools/LevelManager/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOZ.Tools.LevelManager
+{
+    internal class LevelManager
+    {
+        /*Location data for level files*/
+        public String levelFileLocation { get; set; }
+
+
+        /*Function to initialize level data structure and fill it in*/
+        public void initialize()
+        {
+            /*Check for proper file location*/
+            if (levelFileLocation == null) 
+            {
+                throw new InvalidOperationException("Level failed to load: Level file location NULL");
+            }
+
+
+            
+
+        
+        }
+
+    }
+}
---
+++ Tools/LevelManager/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOZ.Tools.LevelManager
+{
+    internal class Room
+    {
+        /*Lists of things in room*/
+
+    }
+}
