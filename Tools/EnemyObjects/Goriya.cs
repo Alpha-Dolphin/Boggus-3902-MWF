8a96726e03ff63cff2c31b39e944d67f00ab5b0c
Ben Elleman <elleman.28@buckeyemail.osu.edu>
2022-11-14 20:52:30 -0500

    Fixed goriya

--- Tools/EnemyObjects/Goriya.cs
+++ Tools/EnemyObjects/Goriya.cs
@@ -7,83 +7,65 @@ using Microsoft.Xna.Framework;
 using SpriteBatch = Microsoft.Xna.Framework.Graphics.SpriteBatch;
 using LOZ.Tools.PlayerObjects;
 using LOZ.Tools.EnemyObjects;
-using LOZ.Tools.Sprites;
 
 namespace LOZ.Tools
 {
-    internal class Goriya : IEnemy, ICollidable
+    internal class Goriya : IEnemy
     {
         Vector2 enemyDirection;
-        EnemyConstants.Direction direction;
-        bool directionChange = false;
         Vector2 enemyPosition;
+
+        readonly GoriyaSprite goriyaSprite;
+
+        readonly EnemyObjects.Boomerang boomerang;
 
         int enemyState;
 
         double stateTime;
-
-        //readonly GoriyaSprite goriyaSprite;
-        AnimatedMovingSprite goriyaSprite;
-
-        //readonly EnemyObjects.Boomerang boomerang;
-        PlayerObjects.Boomerang boomerang;
 
         readonly Random rand;
 
         double moveCheck;
         double moveTime;
         double moveProb;
-        public void SetHurtbox(Rectangle rect)
-        {
-            enemyPosition.Y = rect.Y;
-            enemyPosition.X = rect.X;
-        }
+
         public Goriya(int X, int Y)
         {
             enemyDirection.X = 0;
             enemyDirection.Y = 0;
-            direction = EnemyConstants.Direction.Down;
 
-            enemyPosition.Y = Y;
             enemyPosition.X = X;
+            enemyPosition.Y = Y;
 
             rand = new();
 
-            stateTime = 0.0;
-
             enemyState = 1;
 
-            goriyaSprite = new AnimatedMovingSprite(Game1.REGULAR_ENEMIES_SPRITESHEET, (int)enemyPosition.X, (int)enemyPosition.Y,
-                EnemyConstants.GORIYA_DOWN);
+            stateTime = 0;
+
+            boomerang = new EnemyObjects.Boomerang();
+
+            goriyaSprite = new GoriyaSprite();
 
             moveCheck = -1;
         }
 
-        public Rectangle GetHurtbox()
+        public Rectangle GetRectangle()
         {
-            Vector2 wH = new Vector2(goriyaSprite.GetDestinationRectangle().Width, goriyaSprite.GetDestinationRectangle().Height);
+            Vector2 wH = goriyaSprite.GetWidthHeight();
             return new Rectangle((int)enemyPosition.X, (int)enemyPosition.Y, (int)wH.X, (int)wH.Y);
         }
 
         public void Attack(GameTime gameTime)
         {
-            Vector2 velocity = new Vector2(0, 0);
-
-            switch (this.direction)
-            {
-                case EnemyConstants.Direction.Up: velocity = new Vector2(0, -1); break;
-                case EnemyConstants.Direction.Left: velocity = new Vector2(-1, 0); break;
-                case EnemyConstants.Direction.Right: velocity = new Vector2(1, 0); break;
-                case EnemyConstants.Direction.Down: velocity = new Vector2(0, 1); break;
-            }
-
-            boomerang = new PlayerObjects.Boomerang(Game1.LINK_SPRITESHEET, this, enemyPosition, PlayerConstants.BOOMERANG_SPEED * velocity);
+            boomerang.Activate(enemyDirection, enemyPosition);
         }
 
         public void Die()
         {
             enemyState = -1;
         }
+
         private void DeleteEnemy()
         {
             Game1.enemyDieList.Add(this);
@@ -91,75 +73,33 @@ namespace LOZ.Tools
 
         public void Move(GameTime gameTime)
         {
-            EnemyConstants.Direction temp;
-            if (boomerang == null)
+            if (boomerang.GetAttackTime() < 0.0)
             {
-                Vector2 delta = new Vector2((float)(enemyDirection.X * gameTime.ElapsedGameTime.TotalMilliseconds / 25), (float)(enemyDirection.Y * gameTime.ElapsedGameTime.TotalMilliseconds / 25));
-                if (Math.Abs(delta.X) > 0)
-                {
-                    temp = delta.X > 0 ? EnemyConstants.Direction.Right : EnemyConstants.Direction.Left;
-                }
-                else if (Math.Abs(delta.Y) > 0)
-                {
-
-                    temp = delta.Y > 0 ? EnemyConstants.Direction.Down : EnemyConstants.Direction.Up;
-                }
-                else
-                {
-                    temp = EnemyConstants.Direction.None;
-                }
-
-                directionChange = !temp.Equals(direction);
-                direction = temp;
-
-                enemyPosition += delta;
+                enemyPosition.X += (float)(enemyDirection.X * gameTime.ElapsedGameTime.TotalMilliseconds / 25);
+                enemyPosition.Y += (float)(enemyDirection.Y * gameTime.ElapsedGameTime.TotalMilliseconds / 25);
             }
         }
 
         public void Draw(SpriteBatch _spriteBatch)
         {
-            goriyaSprite.Draw(_spriteBatch);
-            if (boomerang != null) boomerang.Draw(_spriteBatch);
+            goriyaSprite.Draw(_spriteBatch, enemyPosition);
+            if (boomerang.GetAttackTime() > 0.0) boomerang.Draw(_spriteBatch);
         }
 
         public void Update(GameTime gameTime)
         {
             stateHandler(gameTime);
-            goriyaSprite.Update((int)enemyPosition.X, (int)enemyPosition.Y);
-            if (enemyState == 0)
+            goriyaSprite.Update(gameTime, enemyState, enemyDirection);
+            if (enemyState == 0 && boomerang.GetAttackTime() < 0.0)
             {
                 MovementUpdate(gameTime);
                 AttackUpdate(gameTime);
             }
-            if (boomerang != null)
+            else if (enemyState == 0)
             {
-                boomerang.Update();
-                if (!boomerang.stillExists()) boomerang = null;
-            }
-
-            if (directionChange)
-            {
-                switch (direction)
-                {
-                    case EnemyConstants.Direction.Up:
-                        goriyaSprite = new AnimatedMovingSprite(Game1.REGULAR_ENEMIES_SPRITESHEET,
-                        (int)enemyPosition.X, (int)enemyPosition.Y, EnemyConstants.GORIYA_UP); break;
-                    case EnemyConstants.Direction.Left:
-                        goriyaSprite = new AnimatedMovingSprite(Game1.REGULAR_ENEMIES_SPRITESHEET,
-                        (int)enemyPosition.X, (int)enemyPosition.Y, EnemyConstants.GORIYA_LEFT); break;
-                    case EnemyConstants.Direction.Right:
-                        goriyaSprite = new AnimatedMovingSprite(Game1.REGULAR_ENEMIES_SPRITESHEET,
-                        (int)enemyPosition.X, (int)enemyPosition.Y, EnemyConstants.GORIYA_RIGHT); break;
-                    case EnemyConstants.Direction.Down:
-                        goriyaSprite = new AnimatedMovingSprite(Game1.REGULAR_ENEMIES_SPRITESHEET,
-                        (int)enemyPosition.X, (int)enemyPosition.Y, EnemyConstants.GORIYA_DOWN); break;
-                    case EnemyConstants.Direction.None:
-                        goriyaSprite = new AnimatedMovingSprite(Game1.REGULAR_ENEMIES_SPRITESHEET,
-                        (int)enemyPosition.X, (int)enemyPosition.Y, new List<Rectangle>() { goriyaSprite.GetSourceRectangle() }); break;
-                }
+                boomerang.Update(gameTime);
             }
         }
-
         private void stateHandler(GameTime gameTime)
         {
             if (enemyState == 1)
@@ -183,6 +123,18 @@ namespace LOZ.Tools
         private void AttackUpdate(GameTime gameTime)
         {
             if (rand.Next() % 4950 <= 25) Attack(gameTime);
+        }
+
+        public void SetHurtbox(Rectangle rect)
+        {
+            enemyPosition.Y = rect.Y;
+            enemyPosition.X = rect.X;
+        }
+
+        public Rectangle GetHurtbox()
+        {
+            Vector2 wH = goriyaSprite.GetWidthHeight();
+            return new Rectangle((int)enemyPosition.X, (int)enemyPosition.Y, (int)wH.X, (int)wH.Y);
         }
 
         private void MovementUpdate(GameTime gameTime)
--- Tools/EnemyObjects/GoriyaSprite.cs
+++ Tools/EnemyObjects/GoriyaSprite.cs
@@ -8,18 +8,20 @@ namespace LOZ.Tools
     internal class GoriyaSprite : ISpriteEnemy
     {
         Rectangle anim;
+        Texture2D currSheet;
+        double timer;
 
         SpriteEffects enemySpriteEffect;
         public void Draw(SpriteBatch _spriteBatch, Vector2 enemyPosition)
         {
             _spriteBatch.Draw(
-                Game1.REGULAR_ENEMIES_SPRITESHEET,
-                enemyPosition,
+                currSheet,
+                enemyPosition * Constants.objectScale * 2,
                 anim,
                 Color.White,
                 0f,
                 new Vector2(anim.Width / 2, anim.Height / 2),
-                2,
+                2 * Constants.objectScale,
                 enemySpriteEffect,
                 0f
             );
@@ -29,17 +31,32 @@ namespace LOZ.Tools
             //Nothing
         }
 
-        public void Update(GameTime gameTime, Vector2 enemyDirection)
+        public void Update(GameTime gameTime, int enemyState, Vector2 enemyDirection)
         {
             Rectangle[] GoriyaFrames = new[] { new Rectangle(222, 11, 16, 16), new Rectangle(239, 11, 16, 16), new Rectangle(256, 11, 16, 16), new Rectangle(273, 11, 16, 16) };
+            if (enemyState == 0)
+            {
+                if (enemyDirection.Y == 0) anim = GoriyaFrames[(int)(gameTime.TotalGameTime.TotalMilliseconds / 100) % 2 + 2];
+                else if (enemyDirection.Y > 0) anim = GoriyaFrames[0];
+                else if (enemyDirection.Y < 0) anim = GoriyaFrames[1];
 
-            //Currently this method does not know about nor care about if the enemy is attacking, which is a good thing, but probably hard to maintain
-            if (enemyDirection.Y == 0) anim = GoriyaFrames[(int)(gameTime.TotalGameTime.TotalMilliseconds / 100) % 2 + 2];
-            else if (enemyDirection.Y > 0) anim = GoriyaFrames[0];
-            else if (enemyDirection.Y < 0) anim = GoriyaFrames[1];
-
-            enemySpriteEffect = enemyDirection.Y != 0 ? (((int)(gameTime.TotalGameTime.TotalMilliseconds / 100) % 2 == 1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None) : (enemyDirection.X < 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
-        }
+                enemySpriteEffect = enemyDirection.Y != 0 ? (((int)(gameTime.TotalGameTime.TotalMilliseconds / 100) % 2 == 1) ? SpriteEffects.FlipHorizontally : SpriteEffects.None) : (enemyDirection.X < 0 ? SpriteEffects.FlipHorizontally : SpriteEffects.None);
+                currSheet = Game1.REGULAR_ENEMIES_SPRITESHEET;
+            }
+            else if (enemyState == 1)
+            {
+                if (gameTime.TotalGameTime.TotalMilliseconds - timer > Constants.enemyEntryExitTime) timer = gameTime.TotalGameTime.TotalMilliseconds;
+                anim = IEnemy.Appear(timer);
+                currSheet = Game1.LINK_SPRITESHEET;
+            }
+            else if (enemyState == -1)
+            {
+                if (gameTime.TotalGameTime.TotalMilliseconds - timer > Constants.enemyEntryExitTime) timer = gameTime.TotalGameTime.TotalMilliseconds;
+                anim = IEnemy.Disappear(timer);
+                currSheet = Game1.EXPLOSION;
+            }
+            timer += gameTime.ElapsedGameTime.TotalMilliseconds;
+        }        
 
         public Vector2 GetWidthHeight()
         {
